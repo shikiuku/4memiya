5cacda21869d55b043154b34335ca0faf8843f8b
kuni567 <kuni567489@gmail.com>
2025-12-20 16:54:22 +0900

    Update search placeholder and revert carousel tweaks

--- src/components/features/search/product-search.tsx
+++ src/components/features/search/product-search.tsx
@@ -40,7 +40,7 @@ export function ProductSearch({ }: Props) {
             <div className="relative">
                 <Search className="absolute left-3 top-1/2 h-4 w-4 -translate-y-1/2 text-slate-400" />
                 <Input
-                    placeholder="キーワードやIDNo.で検索"
+                    placeholder="キーワードで検索"
                     className="pl-10 w-[200px] md:w-[300px]"
                     value={term}
                     onChange={(e) => setTerm(e.target.value)}
