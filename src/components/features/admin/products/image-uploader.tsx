76f99f0c56d7e4f2fb7f43bf4e630f9b6f12a4c3
kuni567 <kuni567489@gmail.com>
2025-12-21 21:23:16 +0900

    Make image delete button always visible

--- src/components/features/admin/products/image-uploader.tsx
+++ src/components/features/admin/products/image-uploader.tsx
@@ -91,7 +91,7 @@ export function ImageUploader({ initialImages = [], onImagesChange }: ImageUploa
                         <button
                             type="button"
                             onClick={() => removeImage(index)}
-                            className="absolute top-1 right-1 bg-black/50 hover:bg-red-600 text-white p-1 rounded-full opacity-0 group-hover:opacity-100 transition-all"
+                            className="absolute top-1 right-1 bg-black/50 hover:bg-red-600 text-white p-1 rounded-full transition-all"
                         >
                             <X className="w-3 h-3" />
                         </button>
