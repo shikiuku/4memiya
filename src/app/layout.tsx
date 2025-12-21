5676a38f42984b9d533b5667594c854fafb19710
kuni567 <kuni567489@gmail.com>
2025-12-21 21:40:54 +0900

    Fix hydration mismatch error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ja">
+    <html lang="ja" suppressHydrationWarning>
       <body className="antialiased min-h-screen flex flex-col">
         {children}
       </body>
